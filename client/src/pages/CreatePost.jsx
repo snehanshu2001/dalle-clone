03536fbeb897583f9499c155494977ced6c20cd7
snehanshu2001 <snehanshu321@gmail.com>
2023-08-12 19:02:51 +0530

    final change

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,7 +14,29 @@ const CreatePost = () => {
   });
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
-  const generateImage = () => {};
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch("http://localhost:8000/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (error) {
+        console.log(error);
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert("please enter a prompt");
+    }
+  };
   const handleSubmit = () => {};
 
   const handleChange = (e) => {
@@ -49,7 +71,7 @@ const CreatePost = () => {
           <FormField
             LableName="Promt"
             type="type"
-            name="promt"
+            name="prompt"
             placeholder="A plush toy robot sitting against a yellow wall"
             value={form.prompt}
             handleChange={handleChange}
